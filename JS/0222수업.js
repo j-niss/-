ed1127e3c9b22a998a8c287761080f1c8ad12994
j-niss <hanin3143@kakao.com>
2023-03-06 00:51:30 +0900

    자바스크립트 복습

---
+++ JS/0222수업.js
@@ -0,0 +1,15 @@
+let name = "오한민" ;
+let name2 = "오유민" ;
+
+alert(name);
+
+const normalItem = 20;
+const eventItem = 40;
+
+userpoint = 0;
+
+if(유저가일반상품구매시){
+    userpoint += normalItem;
+}else if(유저가이벤트상품구매시){
+    usertpoint += eventItem;
+}
